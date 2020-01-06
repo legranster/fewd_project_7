7eab52019016d02b879db50b11b79471fd3260fc
Manuel Legrand <nuelegrand@gmail.com>
2020-01-05 21:04:51 -0500

    add jquery for autocomplete, add Exceeds Expectations criteria

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 const alertBanner = document.getElementById("alert");
-const messageForm = document.getElementById('messageUserForm');
+const notificationBell = document.getElementById('notificationBell');
+const notificationDiv = notificationBell.parentNode;
+const notificationTray = document.getElementById('notifications');
+const notificationBanners = document.getElementsByClassName('notificationAlert');
+const chartSelector = document.getElementById('traffic-nav');
+const chartOptions = document.getElementsByClassName('traffic-nav-link');
 const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
 const alert = document.getElementById('messageSent');
+const userList = ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'];
+
+let emailSetting = document.getElementById('email');
+let profileSetting = document.getElementById('profile');
+let timezoneSetting = document.getElementById('timezone');
+
+const saveButton = document.getElementById('save');
+const cancelButton = document.getElementById('cancel');
 
 // Chart.JS Content -----------//
 const trafficCanvas = document.getElementById("traffic-chart");
 const dailyCanvas = document.getElementById('daily-chart');
 const mobileCanvas = document.getElementById('mobile-chart');
-let trafficData = {
+let hourlyData = {
+    labels: ['08:00AM', '09:00AM', '10:00AM', '11:00AM', '12:00PM', '01:00PM', '02:00PM', '03:00PM', '04:00PM', '05:00PM', '06:00PM', '07:00PM'],
+    datasets: [{
+        data: [0, 5, 4, 10, 12, 15, 13, 10, 18, 20, 22, 15],
+        backgroundColor: '#499797',
+        borderWidth: 1,
+    }]
+};
+let weeklyData = {
     labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
     datasets: [{
         data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
@@ -17,6 +38,14 @@ let trafficData = {
         borderWidth: 1,
     }]
 };
+let monthlyData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'September', 'October', 'November', 'December'],
+    datasets: [{
+        data:[4500, 5000, 4000, 6500, 5500, 6000, 7000, 6000, 8000, 7000, 9000, 9000],
+        backgroundColor: '#499797',
+        borderWidth: 1,
+    }]
+}
 let trafficOptions = {
     aspectRatio: 2.5,
     animation: {
@@ -35,7 +64,7 @@ let trafficOptions = {
 };
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficData,
+    data: weeklyData,
     options: trafficOptions
 });
 let dailyData = {
@@ -95,6 +124,59 @@ let mobileChart = new Chart(mobileCanvas, {
     options: mobileOptions
 });
 
+//Traffic Chart Function to change data
+
+chartSelector.addEventListener('click', (e) => {
+    let text = e.target.textContent;
+    for (let i = 0; i < chartOptions.length; i++){
+        chartOptions[i].classList = 'traffic-nav-link';
+    }
+    if (text === "Hourly"){
+        chartOptions[0].classList = "traffic-nav-link active";
+        trafficChart = new Chart(trafficCanvas, {
+            type: 'line',
+            data: hourlyData,
+            options: trafficOptions
+        });
+    } else if (text === 'Daily'){
+        chartOptions[1].classList = "traffic-nav-link active";
+        trafficChart = new Chart(trafficCanvas, {
+            type: 'line',
+            data: dailyData,
+            options: trafficOptions
+        });
+    } else if (text === 'Weekly'){
+        chartOptions[2].classList = "traffic-nav-link active";
+        trafficChart = new Chart(trafficCanvas, {
+            type: 'line',
+            data: weeklyData,
+            options: trafficOptions
+        });
+    } else if (text === 'Monthly'){
+        chartOptions[3].classList = "traffic-nav-link active";
+        trafficChart = new Chart(trafficCanvas, {
+            type: 'line',
+            data: monthlyData,
+            options: trafficOptions
+        });
+    }
+});
+
+// notification tray Event Handler 
+
+notificationBell.addEventListener('click', (e) => {
+    if (notificationTray.style.display === ""){
+        notificationTray.style.display = "block";
+    } else if (notificationTray.style.display === "block"){
+        notificationTray.style.display = "";
+    }
+})
+
+notificationTray.addEventListener('click', (e) => {
+    if(e.target.classList.contains('alert-banner-close')){
+        e.target.parentNode.style.display = 'none';
+    }
+});
 
 
 // Alert Banner HTML and event handler 
@@ -108,6 +190,10 @@ alertBanner.addEventListener('click', (e) => {
 });
 
 // Messaging the User Section
+
+$('#userField').autocomplete({
+    source: userList,
+});
 
 send.addEventListener('click', (e) => {
     e.preventDefault();
@@ -127,3 +213,24 @@ alert.addEventListener('click', (e) => {
         alert.innerHTML="";
     }
 });
+
+// Local Storage funtimes
+
+saveButton.addEventListener('click', (e) => {
+    let isEmailChecked = emailSetting.checked;
+    let isProfileSettingChecked = profileSetting.checked;
+    let timezoneSettingSelected = timezoneSetting.value;
+    localStorage.setItem('email', isEmailChecked);
+    localStorage.setItem('profile', isProfileSettingChecked);
+    localStorage.setItem('timezone', timezoneSettingSelected);
+});
+
+cancelButton.addEventListener('click', (e) => {
+    localStorage.setItem('email', false);
+    localStorage.setItem('profile', false);
+    localStorage.setItem('timezone', "00000");
+});
+
+emailSetting.checked = JSON.parse(localStorage.getItem('email'));
+profileSetting.checked = JSON.parse(localStorage.getItem('profile'));
+timezoneSetting.value = localStorage.getItem('timezone');
